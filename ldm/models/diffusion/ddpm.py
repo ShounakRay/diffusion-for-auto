22122caed1a297780900257c7d99bcd5567966bc
Shounak Ray <shounak@stanford.edu>
2023-10-18 14:44:02 -0700

    possible bugfix

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1027,6 +1027,9 @@ class LatentDiffusion(DDPM):
         loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
         loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
 
+        # NOTE: [SHOUNAK] Added by shounak based on:
+        # https://github.com/AUTOMATIC1111/stable-diffusion-webui/issues/3958#issuecomment-1331284615
+        self.logvar = self.logvar.to(self.device)
         logvar_t = self.logvar[t].to(self.device)
         loss = loss_simple / torch.exp(logvar_t) + logvar_t
         # loss = loss_simple / torch.exp(self.logvar) + self.logvar
