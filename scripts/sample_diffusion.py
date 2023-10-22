d4225f222161d6e8b5a5a7ba26ef67533912b161
Shounak Ray <shounak@stanford.edu>
2023-10-22 16:27:18 -0700

    Update sample_diffusion.py

--- scripts/sample_diffusion.py
+++ scripts/sample_diffusion.py
@@ -239,7 +239,7 @@ def load_model(config, ckpt, gpu, eval_mode):
         print(f"Loading model from {ckpt}")
         pl_sd = torch.load(ckpt, map_location="cpu")
         # global_step = pl_sd["global_step"]
-        global_step = pl_sd.get("global_step", None)
+        global_step = pl_sd.get("global_step", 0)
     else:
         pl_sd = {"state_dict": None}
         global_step = None
