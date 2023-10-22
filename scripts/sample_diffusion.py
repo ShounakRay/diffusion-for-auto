5ca611520216556ee98936950fb536c522c52f91
Shounak Ray <shounak@stanford.edu>
2023-10-22 16:21:17 -0700

    global_step protection

--- scripts/sample_diffusion.py
+++ scripts/sample_diffusion.py
@@ -238,7 +238,8 @@ def load_model(config, ckpt, gpu, eval_mode):
     if ckpt:
         print(f"Loading model from {ckpt}")
         pl_sd = torch.load(ckpt, map_location="cpu")
-        global_step = pl_sd["global_step"]
+        # global_step = pl_sd["global_step"]
+        global_step = pl_sd.get(global_step, None)
     else:
         pl_sd = {"state_dict": None}
         global_step = None
